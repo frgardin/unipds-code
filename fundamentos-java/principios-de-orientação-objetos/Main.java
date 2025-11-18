a17eec0169eb1708ffb7b0b0e901611ace95e8f9
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-11-18 20:46:39 -0300

    1 principio de orientacao de objetos

---
+++ fundamentos-java/principios-de-orientação-objetos/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Produto p = new Produto(1, "desc", 1.0f, 50);
+
+        System.out.println(p);
+        
+    }
+}
---
+++ fundamentos-java/principios-de-orientação-objetos/Produto.java
@@ -0,0 +1,13 @@
+public class Produto {
+    int codigo;
+    String descricao;
+    float preco;
+    int estoque;
+
+    Produto (int codigo, String descricao, float preco, int estoque) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+}
