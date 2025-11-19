85bd3765b9cce8ffacf23648d19e0c071eb94ffb
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-11-18 23:12:27 -0300

    definindo pacotes

---
+++ fundamentos-java/principios-de-orientação-objetos/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import core.Produto;
+
+public class Main {
+    public static void main(String[] args) {
+        Produto p = new Produto(1, "desc", 1.0f, 50);
+
+        System.out.println(p);
+        
+    }
+}
