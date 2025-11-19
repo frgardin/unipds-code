1e72afa04725ff672d8f5c1f6cd1fa2b7351312e
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-11-18 23:02:50 -0300

    create contabancaria class

---
+++ fundamentos-java/principios-de-orientação-objetos/ContaBancaria.java
@@ -0,0 +1,34 @@
+public class ContaBancaria {
+    private int numero;
+    private String titular;
+    private double saldo;
+
+    public ContaBancaria(int numero, String titular) {
+        this.numero = numero;
+        this.titular = titular;
+    }
+
+    public void creditar(double valor) {
+        this.saldo += valor;
+    }
+
+    public boolean debitar(double valor) {
+        if (saldo >= valor) {
+            saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
