2288e3ca2f4aee9496a1ab4e3df6683935967d7a
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-11-20 10:24:39 -0300

    set sem equals

--- fundamentos-java/estruturas-de-dados/src/Main.java
+++ fundamentos-java/estruturas-de-dados/src/Main.java
@@ -1,12 +1,17 @@
 void main() {
     List<Produto> list = new ArrayList<>();
 
-
-
     list.add(new Produto(1, "p1", 1.00));
     list.add(new Produto(2, "p2", 2.00));
     list.add(new Produto(3, "p3", 3.00));
     list.add(new Produto(4, "p4", 4.00));
         
-    IO.println(list);
+    IO.println("List: " + list);
+
+    Set<Produto> set = new HashSet<>();
+
+    set.add(new Produto(1, "p1", 1.00));
+    set.add(new Produto(1, "p1", 1.00));
+
+    IO.println("Set: " + set);
 }
