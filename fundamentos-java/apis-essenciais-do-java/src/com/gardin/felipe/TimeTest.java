7015d53372c6fd1a48c86ed925bf2ecb3248e62e
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-12-11 20:25:33 -0300

    essential apis

--- fundamentos-java/apis-essenciais-do-java/src/com/gardin/felipe/TimeTest.java
+++ fundamentos-java/apis-essenciais-do-java/src/com/gardin/felipe/TimeTest.java
@@ -2,6 +2,7 @@ package com.gardin.felipe;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 
 public class TimeTest {
@@ -14,5 +15,9 @@ public class TimeTest {
 		System.out.println(ld);
 		System.out.println(lt);
 		System.out.println(ldt);
+		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy'T'HH:mm:ss.SSSSSSSSS");
+		System.out.println(ldt.format(dtf));
+		
 	}
 }
